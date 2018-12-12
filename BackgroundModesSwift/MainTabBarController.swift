52bfeb7df733ecbee324d868fa865e6f590e7948
Alex <therebeloper@gmail.com>
2018-12-12 22:37:43 +0200

    Initial Commit

---
+++ BackgroundModesSwift/MainTabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabBarController.swift
+//  BackgroundModesSwift
+//
+//  Created by Alex Nagy on 12/12/2018.
+//  Copyright © 2018 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
